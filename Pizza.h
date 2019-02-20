f0fe7eea46b45eeb899b71d1ca1aa8db19987ab1
Tifzou <36513830+Tifzou@users.noreply.github.com>
2019-02-20 16:07:39 +0100

    pizza definition

--- Pizza.h
+++ Pizza.h
@@ -4,6 +4,13 @@
 
 class Pizza
 {
+    // attributes
+public:
+    int R; // nb of rows in the pizza
+    int C; // nb of columns in the pizza
+    int grid [R][C]; // pizza cells
+    
+    // methods
 public:
     Pizza();
 };
